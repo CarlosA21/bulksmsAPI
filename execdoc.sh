1eb67898e9766bfadc1b1e836c54b0c2386f7557
Carlos de los santos <48720295+CarlosA21@users.noreply.github.com>
2025-09-14 11:44:02 -0400

    Fix formatting of environment variables in execdoc.sh

--- execdoc.sh
+++ execdoc.sh
@@ -9,9 +9,9 @@ sudo docker run -d \
   -e SPRING_DATASOURCE_URL=jdbc:mysql://bulksms-mysql:3306/bulksmsdb \
   -e SPRING_DATASOURCE_USERNAME=bulksmsuser \
   -e SPRING_DATASOURCE_PASSWORD=bulksmspass \
-  -e paypal.mode=sandbox   # "sandbox" para pruebas, "live" en producción
-  -e paypal.client.id=AXmZKG8wynj4Y0Icu_f68I1YHzT6olqUiF_NXQL9z2YhApGGWLuZI6KhFdXgZnsa1Jsfy1vU-cOzliSi
-  -e paypal.client.secret=AXmZKG8wynj4Y0Icu_f68I1YHzT6olqUiF_NXQL9z2YhApGGWLuZI6KhFdXgZnsa1Jsfy1vU-cOzliSi
+  -e paypal.mode=sandbox   # "sandbox" para pruebas, "live" en producción \
+  -e paypal.client.id=AXmZKG8wynj4Y0Icu_f68I1YHzT6olqUiF_NXQL9z2YhApGGWLuZI6KhFdXgZnsa1Jsfy1vU-cOzliSi \
+  -e paypal.client.secret=AXmZKG8wynj4Y0Icu_f68I1YHzT6olqUiF_NXQL9z2YhApGGWLuZI6KhFdXgZnsa1Jsfy1vU-cOzliSi \
   --restart unless-stopped \
   carlosa21/bulksms-api:latest
 
